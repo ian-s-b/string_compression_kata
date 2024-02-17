b0d27974d39992cefa8ecc78d3732ff37ce5c607
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 17:28:13 +0100

    Added parametrize decorator

--- test_string_compressor.py
+++ test_string_compressor.py
@@ -1,6 +1,9 @@
+import pytest
 from string_compressor import compress_str
 
-def test_can_compress():
-    compressed_string = compress_str("aabcccaaa")
-
-    assert compressed_string == "a2b1c3a3"
+@pytest.mark.parametrize(
+        ("str_to_compress", "expected_compressed_string"),
+        [("aabcccaaa", "a2b1c3a3"), ("abc", "a1b1c1"), ("aaa", "a3"), ("kzzzi", "kz3i"), ("", "")])
+def test_compress_str(str_to_compress, expected_compressed_string):
+    compressed_string = compress_str(str_to_compress)
+    assert compressed_string == expected_compressed_string
