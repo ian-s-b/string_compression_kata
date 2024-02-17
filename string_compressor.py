9d93f57e78d71647adaa165b5bebc5e835a0ba67
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 22:43:55 +0100

    Refacto and docstring

--- string_compressor.py
+++ string_compressor.py
@@ -1,10 +1,19 @@
 import re
-from typing import List
+from typing import List, Optional
 
 
 class StringCompressor:
     @staticmethod
     def _check_str_input(str_to_compress: str) -> None:
+        """
+        Method used to check if the string input valid.
+
+        :param str_to_compress: The string that will be compressed.
+        :type str_to_compress: str
+
+        :raises TypeError: If the string to compress type is not correct.
+        :raises ValueError: If the string to compress value contains number or empty spaces.
+        """
         if not isinstance(str_to_compress, str):
             raise TypeError("The input to compress should be a string.")
 
@@ -14,15 +23,25 @@ class StringCompressor:
             raise ValueError("The input to compress should not contain white spaces or numbers.")
 
     @classmethod
-    def compress_str(cls, str_to_compress: str, lower_compression: bool = False) -> str:
+    def compress_str(cls, str_to_compress: str, is_case_sensitive: bool = True) -> str:
+        """
+        The compressed string method.
+
+        :param str_to_compress: The string that will be compressed.
+        :type str_to_compress: str
+        :param is_case_sensitive: Lower output if not case-sensitive.
+        :type is_case_sensitive: bool
+        :return: The compressed string.
+        :rtype: str
+        """
         cls._check_str_input(str_to_compress)
 
-        previous_char: str = None
+        previous_char: Optional[str] = None
         str_count: int = 0
         compressed_chars: List[str] = []
 
         for char in str_to_compress:
-            if lower_compression:
+            if not is_case_sensitive:
                 char = char.lower()
             if previous_char is not None and char != previous_char:
                 compressed_chars.append(previous_char + str(str_count))
