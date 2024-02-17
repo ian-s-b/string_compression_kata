b103498b4b01fb882d29bd116624e40dc16331a2
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 19:52:09 +0100

    [todo 3] Updated unit tests for case sensitive options

--- test_string_compressor.py
+++ test_string_compressor.py
@@ -3,7 +3,7 @@ from string_compressor import compress_str
 
 @pytest.mark.parametrize(
         ("str_to_compress", "expected_compressed_string"),
-        [("aabcccaaa", "a2b1c3a3"), ("abc", "abc"), ("aaa", "a3"), ("kzzzi", "k1z3i1"), ("", ""), ("a", "a")])
+        [("aabcccaaa", "a2b1c3a3"), ("abc", "abc"), ("aaa", "a3"), ("kzzzi", "k1z3i1"), ("", ""), ("a", "a"), ("AAbcccAAA", "A2b1c3A3")])
 def test_valid_compression(str_to_compress, expected_compressed_string):
     compressed_string = compress_str(str_to_compress)
     assert compressed_string == expected_compressed_string
@@ -17,3 +17,13 @@ def test_type_error(str_to_compress):
 def test_invalid_str(str_to_compress):
     with pytest.raises(ValueError):
         compress_str(str_to_compress)
+
+@pytest.mark.parametrize(
+        ("str_to_compress", "expected_compressed_string_lower", "expected_compressed_string"),
+        [("aabcccaaa", "a2b1c3a3", "a2b1c3a3"), ("AAbcccAAA", "a2b1c3a3", "A2b1c3A3"), ("AABCCCAAA", "a2b1c3a3", "A2B1C3A3")])
+def test_case_sensitive_options(str_to_compress, expected_compressed_string_lower, expected_compressed_string):
+    compressed_string_lower = compress_str(str_to_compress, True)
+    assert compressed_string_lower == expected_compressed_string_lower
+
+    compressed_string = compress_str(str_to_compress, False)
+    assert compressed_string == expected_compressed_string
