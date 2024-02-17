d54c674cfae5ebbfb0637083a88e09a408ad235e
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 18:49:47 +0100

    [todo 3] Added join usage instead of + operator

--- string_compressor.py
+++ string_compressor.py
@@ -1,11 +1,13 @@
+from typing import List
+
 def compress_str(str_to_compress: str) -> str:
     previous_char: str = None
     str_count: int = 0
-    compressed_str: str = ""
+    compressed_str: List[str] = []
 
     for char in str_to_compress:
         if previous_char is not None and char != previous_char:
-            compressed_str += previous_char + str(str_count)
+            compressed_str.append(previous_char + str(str_count))
             str_count = 1
         else:
             str_count += 1
@@ -13,6 +15,6 @@ def compress_str(str_to_compress: str) -> str:
         previous_char = char
 
     if previous_char is not None:
-        compressed_str += previous_char + str(str_count)
+        compressed_str.append(previous_char + str(str_count))
 
-    return compressed_str
+    return ''.join(compressed_str)
