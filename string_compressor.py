8e2803c1e4e15e2c8a18a2a60d5c134e5108b248
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 21:44:15 +0100

    Refacto to static methods

--- string_compressor.py
+++ string_compressor.py
@@ -2,38 +2,43 @@ import re
 from typing import List
 
 
-def _chek_str_input(str_to_compress: str) -> None:
-    if not isinstance(str_to_compress, str):
-        raise TypeError("The input to compress should be a string.")
+class StringCompressor:
+    def __init__(self):
+        pass
 
-    re_pattern = re.compile(r'[\d\s]')
+    @staticmethod
+    def _check_str_input(str_to_compress: str) -> None:
+        if not isinstance(str_to_compress, str):
+            raise TypeError("The input to compress should be a string.")
 
-    if re_pattern.search(str_to_compress):
-        raise ValueError("The input to compress should not contain white spaces or numbers.")
+        re_pattern = re.compile(r'[\d\s]')
 
+        if re_pattern.search(str_to_compress):
+            raise ValueError("The input to compress should not contain white spaces or numbers.")
 
-def compress_str(str_to_compress: str, lower_compression: bool = False) -> str:
-    _chek_str_input(str_to_compress)
+    @classmethod
+    def compress_str(cls, str_to_compress: str, lower_compression: bool = False) -> str:
+        cls._check_str_input(str_to_compress)
 
-    previous_char: str = None
-    str_count: int = 0
-    compressed_chars: List[str] = []
+        previous_char: str = None
+        str_count: int = 0
+        compressed_chars: List[str] = []
 
-    for char in str_to_compress:
-        if lower_compression:
-            char = char.lower()
-        if previous_char is not None and char != previous_char:
+        for char in str_to_compress:
+            if lower_compression:
+                char = char.lower()
+            if previous_char is not None and char != previous_char:
+                compressed_chars.append(previous_char + str(str_count))
+                str_count = 1
+            else:
+                str_count += 1
+
+            previous_char = char
+
+        if previous_char is not None:
             compressed_chars.append(previous_char + str(str_count))
-            str_count = 1
-        else:
-            str_count += 1
 
-        previous_char = char
+        if len(compressed_chars) < len(str_to_compress):
+            return ''.join(compressed_chars)
 
-    if previous_char is not None:
-        compressed_chars.append(previous_char + str(str_count))
-
-    if len(compressed_chars) <  len(str_to_compress):
-        return ''.join(compressed_chars)
-
-    return str_to_compress
+        return str_to_compress
--- test_string_compressor.py
+++ test_string_compressor.py
@@ -1,29 +1,29 @@
 import pytest
-from string_compressor import compress_str
+from string_compressor import StringCompressor
 
 @pytest.mark.parametrize(
         ("str_to_compress", "expected_compressed_string"),
         [("aabcccaaa", "a2b1c3a3"), ("abc", "abc"), ("aaa", "a3"), ("kzzzi", "k1z3i1"), ("", ""), ("a", "a"), ("AAbcccAAA", "A2b1c3A3")])
 def test_valid_compression(str_to_compress, expected_compressed_string):
-    compressed_string = compress_str(str_to_compress)
+    compressed_string = StringCompressor.compress_str(str_to_compress)
     assert compressed_string == expected_compressed_string
 
 @pytest.mark.parametrize(("str_to_compress"), [None, 1, ["Test"]])
 def test_type_error(str_to_compress):
     with pytest.raises(TypeError):
-        compress_str(str_to_compress)
+        StringCompressor.compress_str(str_to_compress)
 
 @pytest.mark.parametrize(("str_to_compress"), ["a2b1c3a3", "1", "2038232"])
 def test_invalid_str(str_to_compress):
     with pytest.raises(ValueError):
-        compress_str(str_to_compress)
+        StringCompressor.compress_str(str_to_compress)
 
 @pytest.mark.parametrize(
         ("str_to_compress", "expected_compressed_string_lower", "expected_compressed_string"),
         [("aabcccaaa", "a2b1c3a3", "a2b1c3a3"), ("AAbcccAAA", "a2b1c3a3", "A2b1c3A3"), ("AABCCCAAA", "a2b1c3a3", "A2B1C3A3")])
 def test_case_sensitive_options(str_to_compress, expected_compressed_string_lower, expected_compressed_string):
-    compressed_string_lower = compress_str(str_to_compress, True)
+    compressed_string_lower = StringCompressor.compress_str(str_to_compress, True)
     assert compressed_string_lower == expected_compressed_string_lower
 
-    compressed_string = compress_str(str_to_compress, False)
+    compressed_string = StringCompressor.compress_str(str_to_compress, False)
     assert compressed_string == expected_compressed_string
