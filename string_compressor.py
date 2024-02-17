764765a78ccac82b4ea1cf6ddd86e720bcdd9327
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 19:53:09 +0100

    [todo 3] Added lower options on string compressor

--- string_compressor.py
+++ string_compressor.py
@@ -12,7 +12,7 @@ def _chek_str_input(str_to_compress: str) -> None:
         raise ValueError("The input to compress should not contain white spaces or numbers.")
 
 
-def compress_str(str_to_compress: str) -> str:
+def compress_str(str_to_compress: str, lower_compression: bool = False) -> str:
     _chek_str_input(str_to_compress)
 
     previous_char: str = None
@@ -20,6 +20,8 @@ def compress_str(str_to_compress: str) -> str:
     compressed_chars: List[str] = []
 
     for char in str_to_compress:
+        if lower_compression:
+            char = char.lower()
         if previous_char is not None and char != previous_char:
             compressed_chars.append(previous_char + str(str_count))
             str_count = 1
