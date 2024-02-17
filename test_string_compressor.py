3d89db88ddc208333c16517fc77c1d0e2826d630
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 17:52:56 +0100

    Minor fix on test

--- test_string_compressor.py
+++ test_string_compressor.py
@@ -3,7 +3,7 @@ from string_compressor import compress_str
 
 @pytest.mark.parametrize(
         ("str_to_compress", "expected_compressed_string"),
-        [("aabcccaaa", "a2b1c3a3"), ("abc", "a1b1c1"), ("aaa", "a3"), ("kzzzi", "kz3i"), ("", "")])
+        [("aabcccaaa", "a2b1c3a3"), ("abc", "a1b1c1"), ("aaa", "a3"), ("kzzzi", "k1z3i"), ("", "")])
 def test_compress_str(str_to_compress, expected_compressed_string):
     compressed_string = compress_str(str_to_compress)
     assert compressed_string == expected_compressed_string
