58c2e64805c715a3f591ba576bfd37b27c335b1b
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 18:19:17 +0100

    [todo 1] Added compress str function

--- string_compressor.py
+++ string_compressor.py
@@ -1,2 +1,18 @@
-def compress_str(str_to_compress: str):
-    return "a2b1c3a3"
+def compress_str(str_to_compress: str) -> str:
+    previous_char: str = None
+    str_count: int = 0
+    compressed_str: str = ""
+
+    for char in str_to_compress:
+        if previous_char is not None and char != previous_char:
+            compressed_str += previous_char + str(str_count)
+            str_count = 1
+        else:
+            str_count += 1
+
+        previous_char = char
+
+    if previous_char is not None:
+        compressed_str += previous_char + str(str_count)
+
+    return compressed_str
