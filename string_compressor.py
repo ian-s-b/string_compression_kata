65e601a860ad7dc959769bd161e0921376c8461b
ian-s-b <ian.limeira@ensta-bretagne.org>
2024-02-17 18:58:22 +0100

    [todo 3] Added compressed lenght verification

--- string_compressor.py
+++ string_compressor.py
@@ -3,11 +3,11 @@ from typing import List
 def compress_str(str_to_compress: str) -> str:
     previous_char: str = None
     str_count: int = 0
-    compressed_str: List[str] = []
+    compressed_chars: List[str] = []
 
     for char in str_to_compress:
         if previous_char is not None and char != previous_char:
-            compressed_str.append(previous_char + str(str_count))
+            compressed_chars.append(previous_char + str(str_count))
             str_count = 1
         else:
             str_count += 1
@@ -15,6 +15,10 @@ def compress_str(str_to_compress: str) -> str:
         previous_char = char
 
     if previous_char is not None:
-        compressed_str.append(previous_char + str(str_count))
+        compressed_chars.append(previous_char + str(str_count))
 
-    return ''.join(compressed_str)
+    compressed_str = str_to_compress
+    if len(compressed_chars) <  len(str_to_compress):
+        compressed_str =  ''.join(compressed_chars)
+        
+    return compressed_str
